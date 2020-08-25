1b883941453d435d816cd604cb8dadb4b80b5155
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-08-25 03:29:44 -0400

    Mapped out basic plan for sheet script. Started on sheet script.

--- sheet.js
+++ sheet.js
@@ -1,3 +1,40 @@
+// Script for collecting the data and then building the sheet by appending child elements of the appropriate kind
+
+// 1. For each input, (don't forget the IMAGE selector) make sure there is a value, and that it is not empty.
+// 1a. If any are empty, alert the user.
+// 1b. For the image input, look for the element that has 'selected_class' as a class. If it isn't present, then 
+// 2. If there are false inputs, then alert the user to it
+// 2a (Stretch goal) - If there is incorrect input, inform the user as such.
+// 3. Once user has passed error checks, create and append elements as needed
+
 const createSheet = function() {
-    const sheet = document.querySelector('')
+    const sheet = document.querySelector('#sheet_content');
+    
+    if(checkForEmpties()) {
+        alert("HALT! You have missed an inquiry on your form.");
+    }
+
+    else {
+        // 
+    }
+}
+
+// Error Checker - returns true if there is a missing input value
+const checkForEmpties = function() {
+    const inputs = document.querySelectorAll('input');
+
+    // Check each input
+    for(const input of inputs) {
+        if(input.value === '') {
+            return false;
+        }
+    }
+
+    // Check the image
+    const checkClass = document.querySelector('.selected_class');
+    if(checkClass === null) {
+        return false;
+    }
+
+    return true;
 }
