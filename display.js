6c4c85de76c90e0cc51f8d24a9d12ebac177b8de
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-08-25 03:57:44 -0400

    Started script on displaying sheet. Adjusted CSS elements for said sheet. Finished error checking function and Name and class elements for character sheet in sheet.js

---
+++ display.js
@@ -0,0 +1,2 @@
+const createButton = document.querySelector('#create');
+const sheetCanvas = document.querySelector('#sheet_content');
--- sheet.js
+++ sheet.js
@@ -1,24 +1,10 @@
 // Script for collecting the data and then building the sheet by appending child elements of the appropriate kind
-
 // 1. For each input, (don't forget the IMAGE selector) make sure there is a value, and that it is not empty.
 // 1a. If any are empty, alert the user.
 // 1b. For the image input, look for the element that has 'selected_class' as a class. If it isn't present, then 
 // 2. If there are false inputs, then alert the user to it
 // 2a (Stretch goal) - If there is incorrect input, inform the user as such.
 // 3. Once user has passed error checks, create and append elements as needed
-
-const createSheet = function() {
-    const sheet = document.querySelector('#sheet_content');
-    
-    if(checkForEmpties()) {
-        alert("HALT! You have missed an inquiry on your form.");
-    }
-
-    else {
-        // 
-    }
-}
-
 // Error Checker - returns true if there is a missing input value
 const checkForEmpties = function() {
     const inputs = document.querySelectorAll('input');
@@ -37,4 +23,31 @@ const checkForEmpties = function() {
     }
 
     return true;
+}
+
+const createSheet = function() {
+    const sheet = document.querySelector('#sheet_content');
+    
+    if(checkForEmpties()) {
+        alert("HALT! You have missed an inquiry on your form.");
+    }
+
+    else {
+        // Title of the Page
+        const title = document.createElement('h1');
+        title.id = 'sheet_title';
+        title.innerText = 'Character Sheet';
+
+        // Name 
+        const charName = document.createElement('p');
+        charName.className = 'sheet_name';
+        charName.innerText = document.querySelector('name_in');
+        sheet.appendChild(charName);
+
+        // Class
+        const charClass = document.createElement('p');
+        charClass.className = 'sheet_char_class';
+        charClass.innerText = document.querySelector('class_in');
+        sheet.appendChild(charClass);
+    }
 }
